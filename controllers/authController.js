0ef7a8d96282b44d0f257ae2f2a7f9a6e3685103
kamol <kamol.somkrai@gmail.com>
2025-02-21 13:19:17 +0700

    debug

--- controllers/authController.js
+++ controllers/authController.js
@@ -51,7 +51,7 @@ const login = async (req, res) => {
     if (!match) {
       return res.status(401).json({ message: "Invalid credentials." });
     }
-
+    console.log(user);
     // สร้าง JWT Token
     const token = jwt.sign(
       {
