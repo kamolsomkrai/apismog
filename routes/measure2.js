c16e8bdd5b69c2a200d83957b18ca0c80e92b543
kamol <kamol.somkrai@gmail.com>
2025-03-04 02:06:46 +0700

    fix

--- routes/measure2.js
+++ routes/measure2.js
@@ -6,10 +6,14 @@ const {
   getMeasure2show,
 } = require("../controllers/measure2Controller");
 
+const {
+  authenticateTokenFromCookies,
+} = require("../middlewares/authenticateToken");
+
 const router = express.Router();
 
 router.get("/", getMeasure2);
 router.put("/", upsertMeasure2);
-router.post("/show", getMeasure2show);
+router.post("/show", authenticateTokenFromCookies, getMeasure2show);
 
 module.exports = router;
