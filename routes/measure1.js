8c2f9392deb3f7e974a8e108f2153fdf86a650e5
kamol <kamol.somkrai@gmail.com>
2025-03-05 12:51:53 +0700

    test auth

--- routes/measure1.js
+++ routes/measure1.js
@@ -4,9 +4,13 @@ const {
   getMeasure1,
   updateMeasure1,
 } = require("../controllers/measure1Controller");
+
+const {
+  authenticateTokenFromCookies,
+} = require("../middlewares/authenticateToken");
 const router = express.Router();
 
 router.get("/", getMeasure1);
-router.post("/", updateMeasure1);
+router.put("/", authenticateTokenFromCookies, updateMeasure1);
 
 module.exports = router;
