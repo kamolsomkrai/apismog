3bc4fdd2b657c7375311cf3bd9a5a66b16c4bc0d
kamol <kamol.somkrai@gmail.com>
2025-03-05 12:30:51 +0700

    test options

--- routes/measure1.js
+++ routes/measure1.js
@@ -9,4 +9,10 @@ const router = express.Router();
 router.get("/", getMeasure1);
 router.put("/", updateMeasure1);
 
+router.options("/", (req, res) => {
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.sendStatus(200);
+});
+
 module.exports = router;
